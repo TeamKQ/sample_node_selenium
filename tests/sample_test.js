5b2a375491c3a7486a209c68f1b2edbcd807f352
TeamKQ <30633609+TeamKQ@users.noreply.github.com>
2017-08-02 10:11:11 -0700

    Update sample_test.js

--- tests/sample_test.js
+++ tests/sample_test.js
@@ -2,8 +2,7 @@ module.exports = {
   "test sample" : function (browser) {
     browser
       .url("http://127.0.0.1:5001")
-      .waitForElementVisible('body', 1000)
-      .assert.containsText('body', 'Hello World')
+      .sleep(40)
       .end();
   }
 };
